ab871392975d03193f65522337cda56f8c609c63
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2023-05-04 17:12:54 +0200

    Add open graph image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+
+const font = fs.promises.readFile(
+  path.join(fileURLToPath(import.meta.url), "../Inter-Medium.ttf")
+);
+
+export default async function og() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ fontFamily: "Inter", fontWeight: 500 }}>
+          The quick brown fox jumped over the lazy dog.
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await font,
+          weight: 500,
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
